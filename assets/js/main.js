e9677e38c71b9a18ad188ca444a704f409e582de
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2022-12-07 10:02:04 +0700

    hello

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,17 @@ focus_search.addEventListener('blur', function(){
     document.getElementsByClassName('group-search')[0].classList.remove('group-search-active');
     document.getElementsByClassName('btn-search')[0].classList.remove('btn-search-active');
 })
+const navMenu = document.getElementsByClassName('js-menu')[0]
+const showMenu = document.getElementsByClassName('js-nav-slide-2')[0]
+const closeMenu = document.getElementsByClassName('js-close-menu')[0]
+const miniMenu = document.getElementsByClassName('nav-select')[0]
+// navMenu.addEventListener('click',function(){
+//     showMenu.classList.add('open')
+// })
+// closeMenu.addEventListener('click',function(){
+//     showMenu.classList.remove('open')
+// })
+navMenu.onclick = () => {showMenu.classList.toggle('open')
+miniMenu.classList.toggle('open')
+}
+// showMenu.classList.toggle('open')
